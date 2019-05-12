92fb5288cd3cc812abd891f561defa40edc74fe4
dblnjsw <46617221+dblnjsw@users.noreply.github.com>
2019-05-12 19:14:54 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,12 +1,26 @@
 import pygame
 import os
 
-WIDTH=1200
-HEIGHT=750
+WIDTH = 1200
+HEIGHT = 750
 
-allSprite=pygame.sprite.Group()
+allSprite = pygame.sprite.Group()
 
-running=True
+running = True
 
-gFolder=os.path.dirname(__file__)
-imFolder=gFolder+"/img"
+gFolder = os.path.dirname(__file__)
+imFolder = gFolder + "/img"
+
+# 216add
+FONT_NAME = 'SimHei'
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+TITLE = 'DOG IS YOU'
+FPS = 60
+ALL_PICTURE_TAG = [ 'rock', 'flag','grass','box','dog']
+INCREASE = 'increase'
+DECREASE = 'decrease'
+M_Y = 'y'
+M_X = 'x'
+GO = 1
+REVERSE = 2
